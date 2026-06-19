477d6b5c9f06df2b6be5c6ab40f58494ca4f20ac
AzizErcoban33 <ercobana@tcd.ie>
2026-06-19 19:42:51 +0300

    update init

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 from app import data
@@ -11,11 +11,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template(
+        'homepage.html',
+        title="Team Portfolio",
+        url=os.getenv("URL"),
+        nav_links=data.NAV_LINKS,
+        active_page="index",
+        team_members=data.TEAM_MEMBERS,
+    )
+
+
+@app.route('/sri-nidhi')
+def sri_nidhi():
+    return render_template(
         'index.html',
         title="Sri Nidhi Kuchana",
         url=os.getenv("URL"),
         nav_links=data.NAV_LINKS,
-        active_page="index",
+        active_page="sri_nidhi",
         about_me=data.ABOUT_ME,
         experiences=data.EXPERIENCES,
         education=data.EDUCATION,
@@ -25,12 +37,39 @@ def index():
 
 
 @app.route('/hobbies')
-def hobbies():
+@app.route('/sri-nidhi/hobbies')
+def sri_nidhi_hobbies():
     return render_template(
         'hobbies.html',
-        title="Hobbies",
+        title="Sri Nidhi Hobbies",
         url=os.getenv("URL"),
         nav_links=data.NAV_LINKS,
-        active_page="hobbies",
+        active_page="sri_nidhi_hobbies",
         hobbies=data.HOBBIES,
+    )
+
+
+@app.route('/aziz')
+def aziz():
+    return render_template(
+        'coming_soon.html',
+        title="Aziz Ercoban",
+        url=os.getenv("URL"),
+        nav_links=data.NAV_LINKS,
+        active_page="aziz",
+        page_title="Aziz Ercoban",
+        message="Aziz's portfolio page will be added in the next branch.",
+    )
+
+
+@app.route('/aziz/hobbies')
+def aziz_hobbies():
+    return render_template(
+        'coming_soon.html',
+        title="Aziz Hobbies",
+        url=os.getenv("URL"),
+        nav_links=data.NAV_LINKS,
+        active_page="aziz_hobbies",
+        page_title="Aziz Hobbies",
+        message="Aziz's hobbies page will be added in a later branch.",
     )
